367da8d07198b9e529b37f18fc38afe116d70ec7
区达明 <756213932@qq.com>
2019-09-24 23:38:10 +0800

    环境搭建完成

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,15 +3,20 @@
 #include "log.h"
 #include "giflib/gif_lib.h"
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_dming_testgif_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++--x-";
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_dming_testgif_MainActivity_testGif(JNIEnv *env, jobject instance, jstring gifPath_) {
+    const char *gifPath = env->GetStringUTFChars(gifPath_, 0);
 
-
-    LOGI("test: GifErrorString=> %s", GifErrorString(D_GIF_ERR_OPEN_FAILED));
-
-
-    return env->NewStringUTF(hello.c_str());
+    int err;
+    GifFileType *gifFileType = DGifOpenFileName(gifPath, &err);//调用源码api里方法，打开gif，返回GifFileType实体
+    LOGI("start GifErrorString: %s", GifErrorString(err));
+    if (DGifSlurp(gifFileType) == GIF_OK) {
+        LOGI("gif SWidth: %d SHeight: %d", gifFileType->SWidth, gifFileType->SHeight);
+        LOGI("gif ImageCount: %d", gifFileType->ImageCount);
+        LOGI("gif SColorResolution: %d", gifFileType->SColorResolution);
+    }
+    DGifCloseFile(gifFileType, &err);
+    LOGI("end GifErrorString: %s", GifErrorString(err));
+    env->ReleaseStringUTFChars(gifPath_, gifPath);
 }
--- app/src/main/java/com/dming/testgif/MainActivity.java
+++ app/src/main/java/com/dming/testgif/MainActivity.java
@@ -1,8 +1,14 @@
 package com.dming.testgif;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 666);
+        }
+        findViewById(R.id.sample_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File(Environment.getExternalStorageDirectory(), "1/test.gif");
+                if (file.exists()) {
+                    testGif(file.getPath());
+                } else {
+                    Toast.makeText(MainActivity.this, "file not exists!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native void testGif(String gifPath);
 }
